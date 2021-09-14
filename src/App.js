8fe7ede6c23e28feaec215ea230b9bb0e6e48a2b
safafa <safaaballagh97@gmail.com>
2021-09-14 11:12:33 +0100

    Make App class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import react from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends react.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
