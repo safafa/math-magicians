9d755ec133703fbf0478d4e8eace1b174a1a5f64
safafa <safaaballagh97@gmail.com>
2021-09-14 03:08:40 +0100

    Styling

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,16 +2,14 @@ import react from 'react';
 
 export default class Calculator extends react.Component {
   render() {
-    const numbers = ['.', 1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => <button type="button" key={number}>{number}</button>);
-    const verticalSymbols = ['/', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol}>{symbol}</button>);
-    const horizontalSymbols = ['%', '+/-', 'AC'].map((symbol) => <button type="button" key={symbol}>{symbol}</button>);
+    const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, '.'].map((number) => <button type="button" key={number} className="innerButton">{number}</button>);
+    const verticalSymbols = ['/', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol} className="cornerButton">{symbol}</button>);
     return (
-      <div>
+      <div className="calculator">
         <p className="screen">0</p>
         <div className="buttons">
           <div className="vertical">{verticalSymbols}</div>
-          <div className="horizontal">{horizontalSymbols}</div>
-          <div className="numbers">{numbers}</div>
+          <div className="inner-container">{numbers}</div>
         </div>
       </div>
     );
