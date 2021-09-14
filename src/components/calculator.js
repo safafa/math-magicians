8fa254c5a0708a6dd97b451f25d2d46a1b4afa97
safafa <safaaballagh97@gmail.com>
2021-09-14 16:58:00 +0100

    Import calculate and create HandleEvent

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,25 +1,35 @@
 import react from 'react';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends react.Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: 0,
+      total: null,
+      next: null,
+      operation: null,
     };
+    this.handleEvent = this.handleEvent.bind(this);
+  }
+
+  handleEvent(event) {
+    this.setState(
+      calculate(this.state, event.target.name),
+    );
   }
 
   render() {
-    const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1].map((number) => <button type="button" key={number} className="innerButton" id={number}>{number}</button>);
-    const verticalSymbols = ['/', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol} className="cornerButton">{symbol}</button>);
+    const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1].map((number) => <button type="button" key={number} className="innerButton" id={number} name={number} onClick={this.handleEvent}>{number}</button>);
+    const verticalSymbols = ['÷', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol} className="cornerButton" name={symbol} onClick={this.handleEvent}>{symbol}</button>);
     return (
       <div className="calculator">
-        <p className="screen">{this.state.result}</p>
+        <p className="screen">{this.state.total}</p>
         <div className="buttons">
           <div className="vertical">{verticalSymbols}</div>
           <div className="inner-container">
             {numbers}
-            <button type="button" key="zero" className="innerButton" id="zero">0</button>
-            <button type="button" key="dot" className="innerButton" id="dot">.</button>
+            <button type="button" key="zero" className="innerButton" id="zero" name={0} onClick={this.handleEvent}>0</button>
+            <button type="button" key="dot" className="innerButton" id="dot" name="." onClick={this.handleEvent}>.</button>
           </div>
         </div>
       </div>
