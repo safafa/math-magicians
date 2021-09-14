c23effa5cebc9e97f5f1725342375f6c92f5e8d8
safafa <safaaballagh97@gmail.com>
2021-09-14 09:22:15 +0100

    Fix dot button

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,15 +1,25 @@
 import react from 'react';
 
 export default class Calculator extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 0,
+    };
+  }
+
   render() {
-    const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, '.'].map((number) => <button type="button" key={number} className="innerButton">{number}</button>);
+    const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map((number) => <button type="button" key={number} className="innerButton">{number}</button>);
     const verticalSymbols = ['/', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol} className="cornerButton">{symbol}</button>);
     return (
       <div className="calculator">
-        <p className="screen">0</p>
+        <p className="screen">{this.state.result}</p>
         <div className="buttons">
           <div className="vertical">{verticalSymbols}</div>
-          <div className="inner-container">{numbers}</div>
+          <div className="inner-container">
+            {numbers}
+            <button type="button" key="dot" className="innerButton" id="dot">.</button>
+          </div>
         </div>
       </div>
     );
