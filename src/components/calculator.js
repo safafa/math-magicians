174a1509645683d614ed616471215e9316a4b244
safafa <safaaballagh97@gmail.com>
2021-09-15 12:32:47 +0100

    Turn Calculator to func and Use Hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,42 +1,37 @@
-import react from 'react';
-import calculate from '../logic/calculate';
+import React, { useState } from 'react';
+import Calculate from '../logic/calculate';
 
-export default class Calculator extends react.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleEvent = this.handleEvent.bind(this);
-  }
+export default function Calculator() {
+  const stateObj = {
+    total: '0',
+    next: null,
+    operation: null,
+  };
 
-  handleEvent(event) {
-    this.setState(
-      calculate(this.state, event.target.name),
-    );
-  }
+  const [obj, setObj] = useState(() => ({ ...stateObj }));
 
-  render() {
-    const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1].map((number) => <button type="button" key={number} className="innerButton" id={number} name={number} onClick={this.handleEvent}>{number}</button>);
-    const verticalSymbols = ['÷', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol} className="cornerButton" name={symbol} onClick={this.handleEvent}>{symbol}</button>);
-    return (
-      <div className="calculator">
-        <p className="screen">
-          <span>{this.state.total}</span>
-          <span>{this.state.operation}</span>
-          <span>{this.state.next}</span>
-        </p>
-        <div className="buttons">
-          <div className="vertical">{verticalSymbols}</div>
-          <div className="inner-container">
-            {numbers}
-            <button type="button" key="zero" className="innerButton" id="zero" name={0} onClick={this.handleEvent}>0</button>
-            <button type="button" key="dot" className="innerButton" id="dot" name="." onClick={this.handleEvent}>.</button>
-          </div>
+  const handleClick = (event) => {
+    const newObj = { ...obj, ...Calculate(obj, event.target.name) };
+    setObj(() => ({ ...newObj }));
+  };
+
+  const numbers = ['AC', '+/-', '%', 9, 8, 7, 6, 5, 4, 3, 2, 1].map((number) => <button type="button" key={number} className="innerButton" id={number} name={number} onClick={handleClick}>{number}</button>);
+  const verticalSymbols = ['÷', 'x', '-', '+', '='].map((symbol) => <button type="button" key={symbol} className="cornerButton" name={symbol} onClick={handleClick}>{symbol}</button>);
+  return (
+    <div className="calculator">
+      <p className="screen">
+        <span>{obj.total}</span>
+        <span>{obj.operation}</span>
+        <span>{obj.next}</span>
+      </p>
+      <div className="buttons">
+        <div className="vertical">{verticalSymbols}</div>
+        <div className="inner-container">
+          {numbers}
+          <button type="button" key="zero" className="innerButton" id="zero" name={0} onClick={handleClick}>0</button>
+          <button type="button" key="dot" className="innerButton" id="dot" name="." onClick={handleClick}>.</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
