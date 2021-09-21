7064df3e1a6662cb6c4fcb1d83260cd9ae3d415b
safafa <safaaballagh97@gmail.com>
2021-09-21 20:00:12 +0100

    Add Navbar component and set Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import react from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/navbar';
 import Calculator from './components/calculator';
 
 class App extends react.Component {
   render() {
     return (
-      <Calculator />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
