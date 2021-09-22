21001a67dc9fc1f53b21e1c4bf09bf0f6807adad
ali abbani <aliox@ahlam.com>
2021-09-22 16:06:25 +0300

    Add /, *, and % to the operrate.test.js file

--- src/tests/operation.test.js
+++ src/tests/operation.test.js
@@ -8,4 +8,16 @@ describe('operate function', () => {
   test('minimizing', () => {
     expect(operate(1, 2, '-')).toBe('-1');
   });
+
+  test('dividing', () => {
+    expect(operate(6, 2, '÷')).toBe('3');
+  });
+
+  test('multiplying', () => {
+    expect(operate(6, 2, 'x')).toBe('12');
+  });
+
+  test('modulating', () => {
+    expect(operate(4, 2, '%')).toBe('0');
+  });
 });
