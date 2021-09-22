0e1abd41093888f3613c089403017c95f7cfae94
safafa <safaaballagh97@gmail.com>
2021-09-22 15:04:32 +0100

    Add snapshpt test for calculator component

--- src/tests/snapshot.test.js
+++ src/tests/snapshot.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { NavLink, BrowserRouter as Router } from 'react-router-dom';
+import Calculator from '../components/calculator';
 
-it('renders correctly', () => {
-  const tree = renderer
-    .create(
-      <Router>
-        <NavLink to="/">Home</NavLink>
-      </Router>,
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Component test snapshot', () => {
+  it('Router renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Router>
+          <NavLink to="/">Home</NavLink>
+        </Router>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Calculator renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Calculator />,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
